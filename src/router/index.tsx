bae911586399ecbe53a088e5209e4a0e15342e5f
minnnnnnngun <20261305@dms.hs.kr>
2026-09-08 19:13:37 +0900

    feature: 내가 쓴 글 라우터 구현

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import MyPostsPage from "../features/posts/components/MyPostsPageModal";
 
 // 현재는 App에서 직접 페이지를 렌더링하며, 추후 라우팅을 확장할 때 사용할 설정이다.
-export const router = createBrowserRouter([]);
+export const router = createBrowserRouter([
+  {
+    path: "/mypage/posts",
+    element: <MyPostsPage />,
+  },
+]);
